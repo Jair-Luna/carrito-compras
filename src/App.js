bdb595fca5dee53e5586d899188c5b011ba0f60f
Jair-Luna <lunajair90@gmail.com>
2021-10-24 12:36:13 -0500

    First function component

--- src/App.js
+++ src/App.js
@@ -1,38 +1,100 @@
-import { Component } from 'react';
+import { Component, useState } from 'react';
 import Navbar from './components/Navbar';
 import Productos from './components/Productos';
 import Layout from './components/Layout';
 import Title from './components/Title';
 
-class App extends Component {
-  state = {
-    productos: [
-      { name: 'Tomate', price: 150, img: 'images/tomate.jpg' },
-      { name: 'Lechuga', price: 200, img: 'images/lechuga.jpg' },
-      { name: 'Arbeja', price: 250, img: 'images/arbejas.jpg' },
-    ],
-    carro: [],
-    esCarroVisible: false,
-  };
+// class App extends Component {
+//   state = {
+//     productos: [
+//       { name: 'Tomate', price: 150, img: 'images/tomate.jpg' },
+//       { name: 'Lechuga', price: 200, img: 'images/lechuga.jpg' },
+//       { name: 'Arbeja', price: 250, img: 'images/arbejas.jpg' },
+//     ],
+//     carro: [],
+//     esCarroVisible: false,
+//   };
 
-  agregarAlCarro = (producto) => {
-    const { carro } = this.state;
+//   agregarAlCarro = (producto) => {
+//     const { carro } = this.state;
 
+//     if (carro.find((x) => x.name === producto.name)) {
+//       const newCarro = carro.map((x) =>
+//         x.name === producto.name
+//           ? {
+//               ...x,
+//               cantidad: x.cantidad + 1,
+//               total: x.price * (x.cantidad + 1),
+//             }
+//           : x
+//       );
+//       return this.setState({ carro: newCarro });
+//     }
+
+//     return this.setState({
+//       carro: this.state.carro.concat({
+//         ...producto,
+//         cantidad: 1,
+//
+//       }),
+//     });
+//   };
+
+//   mostrarCarro = () => {
+//     if (!this.state.carro.length) {
+//       return;
+//     }
+//     this.setState({ esCarroVisible: !this.state.esCarroVisible });
+//   };
+
+//   render() {
+//     const { esCarroVisible } = this.state;
+//     return (
+//       <div>
+//         <Navbar
+//           carro={this.state.carro}
+//           esCarroVisible={esCarroVisible}
+//           mostrarCarro={this.mostrarCarro}
+//         />
+//         <Layout>
+//           <Title />
+//           <Productos
+//             agregarAlCarro={this.agregarAlCarro}
+//             productos={this.state.productos}
+//           />
+//         </Layout>
+//       </div>
+//     );
+//   }
+// }
+
+const App = () => {
+  const [productos] = useState([
+    { name: 'Tomate', price: 150, img: 'images/tomate.jpg' },
+    { name: 'Lechuga', price: 200, img: 'images/lechuga.jpg' },
+    { name: 'Arbeja', price: 250, img: 'images/arbejas.jpg' },
+  ]);
+
+  const [carro, setCarro] = useState([]);
+
+  const [esCarroVisible, setVisible] = useState(false);
+
+  const agregarAlCarro = (producto) => {
     if (carro.find((x) => x.name === producto.name)) {
       const newCarro = carro.map((x) =>
         x.name === producto.name
           ? {
               ...x,
               cantidad: x.cantidad + 1,
-              total: x.price * (x.cantidad + 1),
+              total: producto.price,
             }
           : x
       );
-      return this.setState({ carro: newCarro });
+      return setCarro({ carro: newCarro });
     }
 
-    return this.setState({
-      carro: this.state.carro.concat({
+    return setCarro({
+      carro: carro.concat({
         ...producto,
         cantidad: 1,
         total: producto.price,
@@ -40,32 +102,28 @@ class App extends Component {
     });
   };
 
-  mostrarCarro = () => {
-    if (!this.state.carro.length) {
+  const mostrarCarro = () => {
+    if (!carro.length) {
       return;
     }
-    this.setState({ esCarroVisible: !this.state.esCarroVisible });
+    setVisible({ esCarroVisible: !esCarroVisible });
   };
 
-  render() {
-    const { esCarroVisible } = this.state;
-    return (
-      <div>
-        <Navbar
-          carro={this.state.carro}
-          esCarroVisible={esCarroVisible}
-          mostrarCarro={this.mostrarCarro}
-        />
-        <Layout>
-          <Title />
-          <Productos
-            agregarAlCarro={this.agregarAlCarro}
-            productos={this.state.productos}
-          />
-        </Layout>
-      </div>
-    );
-  }
-}
+  console.log(productos, carro);
+
+  return (
+    <div>
+      <Navbar
+        carro={carro}
+        esCarroVisible={esCarroVisible}
+        mostrarCarro={mostrarCarro}
+      />
+      <Layout>
+        <Title />
+        <Productos agregarAlCarro={agregarAlCarro} productos={productos} />
+      </Layout>
+    </div>
+  );
+};
 
 export default App;
